ab69106e15bf137aab527b9d75c3011c63c7e3f7
ASIAZURUR <93027041+ASIAZURUR@users.noreply.github.com>
2021-12-21 17:34:53 +0530

    change

--- src/model/Bookdata.js
+++ src/model/Bookdata.js
@@ -1,7 +1,7 @@
 //Accessing Mongoose Package
 const mongoose=require('mongoose');
 //Database connection
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb+srv://userone:userone@asiafiles.w7kkj.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 const Schema=mongoose.Schema;
 //Schema definition
 const BookSchema=new Schema({
